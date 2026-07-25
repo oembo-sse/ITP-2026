535b57517d54492b206c74e392e16ca2c2e62bb0
oeb25 <oliverboving@gmail.com>
2026-07-25 15:32:57 +0100

    update import of pgcl

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
-import pgcl from "pgcl";
+import "pgcl";
 
-await pgcl();
+// await pgcl();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
